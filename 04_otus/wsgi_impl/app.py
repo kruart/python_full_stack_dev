77dac05a2c0f52e66ac1578206a43b3b3b66cfe4
we0z <weoz@ukr.net>
2018-05-25 19:49:17 +0300

    hw04: refactoring

--- 04_otus/wsgi_impl/app.py
+++ 04_otus/wsgi_impl/app.py
@@ -1,18 +1,18 @@
-import config
+from config import ROUTES, HttpStatusCode
 import utils
 
 
 def application(environ, start_response):
     path = environ['PATH_INFO']
-    headers = [('Content-Type', 'text/html')]
+    headers = [('Content-Type', 'text/html; charset=utf-8')]
 
-    page_name = config.ROUTES.get(path, None)
+    page_name = ROUTES.get(path, None)
 
     if page_name:
-        status = config.HttpStatusCode.OK.value
+        status = HttpStatusCode.OK.value
         content = utils.get_content(page_name)
     else:
-        status = config.HttpStatusCode.NOT_FOUND.value
+        status = HttpStatusCode.NOT_FOUND.value
         content = b'Error occurred'
     start_response(status, headers)
     return [content]
