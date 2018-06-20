77b2b5b6a15a5114053cc6e3218734ba295c57a4
we0z <weoz@ukr.net>
2018-06-20 17:46:21 +0300

    09 products in-memory

---
+++ 09_otus/api/db.py
@@ -0,0 +1,21 @@
+
+PRODUCTS = [
+    {
+        "title": "Apple MacBook Pro 13",
+        "price_uah": 37000,
+        "product_image": "/images/products/macbookpro.png",
+        "in_store": True
+    },
+{
+        "title": "ASUS ZenBook UX330UA",
+        "price_uah": 29450,
+        "product_image": "/images/products/zenbook.png",
+        "in_store": True
+    },
+{
+        "title": "Xiaomi Mi Notebook Air",
+        "price_uah": 19999,
+        "product_image": "/images/products/mi.png",
+        "in_store": False
+    }
+]
