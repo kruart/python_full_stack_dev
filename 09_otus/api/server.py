fa78f8d5877993cf1719bfe2f2f4911cc07c08e4
we0z <weoz@ukr.net>
2018-06-21 18:04:47 +0300

    09 add product endpoint

--- 09_otus/api/server.py
+++ 09_otus/api/server.py
@@ -1,12 +1,21 @@
-from flask import Flask, jsonify
+import json
+
+from flask import Flask, jsonify, request
 from db import PRODUCTS
 
 app = Flask(__name__)
 
 
 @app.route('/list_products/')
-def list_products():
+def list_products_handle():
     return jsonify(PRODUCTS)
+
+
+@app.route('/add_product/', methods=['POST'])
+def add_product_handle():
+    raw_product = json.loads(request.data.decode('utf-8'))
+    PRODUCTS.append(raw_product)
+    return jsonify(raw_product)
 
 
 if __name__ == '__main__':
