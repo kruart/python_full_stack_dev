9c266ecae63ebdbb4cd74a529953c3075fc53116
we0z <weoz@ukr.net>
2018-06-21 18:07:34 +0300

    09 refactoring delete verbs from names

--- 09_otus/api/server.py
+++ 09_otus/api/server.py
@@ -6,12 +6,12 @@ from db import PRODUCTS
 app = Flask(__name__)
 
 
-@app.route('/list_products/')
+@app.route('/products/')
 def list_products_handle():
     return jsonify(PRODUCTS)
 
 
-@app.route('/add_product/', methods=['POST'])
+@app.route('/product/', methods=['POST'])
 def add_product_handle():
     raw_product = json.loads(request.data.decode('utf-8'))
     PRODUCTS.append(raw_product)
