e5dc9406afa01e76c5ad2ef0bdb746b7935897c1
we0z <weoz@ukr.net>
2018-06-22 14:52:50 +0300

    09 add filter

--- 09_otus/api/server.py
+++ 09_otus/api/server.py
@@ -13,6 +13,10 @@ def list_products_handle():
         products_to_show = PRODUCTS
         if query:
             products_to_show = [p for p in PRODUCTS if query.lower() in p['title'].lower()]
+
+        filter_field = request.args.get('filter')
+        if filter_field:
+            products_to_show = [p for p in products_to_show if p.get(filter_field)]
         return jsonify(products_to_show)
     else:
         raw_product = json.loads(request.data.decode('utf-8'))
