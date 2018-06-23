0f374d57c17d6afd81c262a586295f7e35c4116f
we0z <weoz@ukr.net>
2018-06-23 16:11:35 +0300

    11 add UserSerializer and hide password

--- 11_otus/drf_blog/blog_api/posts/serializers.py
+++ 11_otus/drf_blog/blog_api/posts/serializers.py
@@ -1,14 +1,24 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 from . import models
 
 
-class PostSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = models.Post
-        fields = '__all__'
+        model = User
+        fields = ('email', 'id', 'username', 'last_name')
+        # exclude = ('password')
 
 
 class TagSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Tag
+        fields = '__all__'
+
+
+class PostSerializer(serializers.ModelSerializer):
+    tags = TagSerializer(many=True, read_only=True)
+    user = UserSerializer(read_only=True)
+    class Meta:
+        model = models.Post
         fields = '__all__'
