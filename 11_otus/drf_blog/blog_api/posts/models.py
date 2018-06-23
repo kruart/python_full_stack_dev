dbd622edc6f3a0c9b56ae8a8001cc015d18a9ea9
we0z <weoz@ukr.net>
2018-06-23 12:48:44 +0300

    11 create models

--- 11_otus/drf_blog/blog_api/posts/models.py
+++ 11_otus/drf_blog/blog_api/posts/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=256)
+    slug = models.SlugField(max_length=256)
+    text = models.TextField()
+    is_published = models.BooleanField(default=False)
+    user = models.ForeignKey('auth.User')
+
+    tags = models.ManyToManyField('Tag', blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=256)
+    slug = models.SlugField(max_length=256)
+
+    def __str__(self):
+        return self.title
