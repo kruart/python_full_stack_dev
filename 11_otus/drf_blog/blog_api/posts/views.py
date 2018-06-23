a48ff66ab79a6b27801428161d59545c59ede0db
we0z <weoz@ukr.net>
2018-06-23 16:25:10 +0300

    11 add basic authentication

--- 11_otus/drf_blog/blog_api/posts/views.py
+++ 11_otus/drf_blog/blog_api/posts/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from . import serializers
 from . import models
 
@@ -6,8 +6,10 @@ from . import models
 class PostViewSet(viewsets.ModelViewSet):
     queryset = models.Post.objects.filter(is_published=True)
     serializer_class = serializers.PostSerializer
+    permission_classes = (permissions.IsAuthenticated,)
 
 
 class TagViewSet(viewsets.ModelViewSet):
     queryset = models.Tag.objects.all()
     serializer_class = serializers.TagSerializer
+    permission_classes = (permissions.IsAuthenticated,)
